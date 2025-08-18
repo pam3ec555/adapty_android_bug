862fdb03c4c08fa5a559908cee06cf7347b94bcd
ramilrakhmatullin <rep555333@gmail.com>
2025-08-18 15:03:01 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'screens/onboardings_view.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: OnboardingsView(),
+    );
+  }
+}
---
+++ lib/screens/onboardings_view.dart
@@ -0,0 +1,43 @@
+import 'package:adapty_flutter/adapty_flutter.dart';
+import 'package:flutter/material.dart';
+
+class OnboardingsView extends StatefulWidget {
+  const OnboardingsView({super.key});
+
+  @override
+  State<OnboardingsView> createState() => _OnboardingsViewState();
+}
+
+class _OnboardingsViewState extends State<OnboardingsView> {
+  AdaptyOnboarding? onboarding;
+
+  @override
+  void initState() {
+    super.initState();
+    init();
+  }
+
+  Future init() async {
+    await Adapty().activate(
+        configuration: AdaptyConfiguration(
+            apiKey: "public_live_i6bLVH4H.2Hy1dsU5ajCAkaRyYgf2"));
+    print("activated get");
+    onboarding = await Adapty().getOnboarding(
+      placementId: "main",
+      locale: "en",
+    );
+    print("onboarding: $onboarding");
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: onboarding == null
+          ? const Center(child: CircularProgressIndicator())
+          : AdaptyUIOnboardingPlatformView(
+              onboarding: onboarding!,
+            ),
+    );
+  }
+}
